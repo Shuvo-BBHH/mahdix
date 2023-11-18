6ed9c039a85c671cdcbd9dae3d6e6ae9d51606b4
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-11-19 01:22:52 +0600

    Create emalple_1.py

---
+++ html_txt/emalple_1.py
@@ -0,0 +1,6 @@
+#-----[sending Request only use Url]-----------
+from mahdix import html_txt
+sop=html_txt('https://www.facebook.com')
+
+print(sop)# Getting only Html all Elements 
+print(sop.text)# Getting only Html
